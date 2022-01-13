34fcd9e2d8c124d252d9eca64511124cbb392d85
Dimitar Stoyanov <97186457+mitacci@users.noreply.github.com>
2022-01-14 00:04:26 +0200

    Create 02_Stacked_Queries.py

---
+++ Stacks_Queues/02_Stacked_Queries.py
@@ -0,0 +1,12 @@
+n = int(imput())
+stack = []
+for _ in range(n):
+  line_parts = input().split()
+  command = line_parts[0]
+  if command == "1":
+    number = int(line_parts[1]
+    stack.append(number)
+  elif command == "2":
+                 
+                 
+              
