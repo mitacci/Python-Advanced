17f9d2d45d20cd554370653142721c938940ca61
Dimitar Stoyanov <97186457+mitacci@users.noreply.github.com>
2022-01-15 02:54:27 +0200

    Create 02_EvenMatrix.py

---
+++ MultidimensionalLists/02_EvenMatrix.py
@@ -0,0 +1,16 @@
+def read_matrix():
+    n = int(input())
+    matrix = []
+    for _ in range(n):
+        row = [int(x) for x in input().split(', ')]
+        matrix.append(row)
+    return matrix
+
+matrix = read_matrix()
+
+
+even_matrix = []
+for row in matrix:
+    only_even = [x for x in row if x % 2 == 0]
+    even_matrix.append(only_even)
+print(even_matrix)
